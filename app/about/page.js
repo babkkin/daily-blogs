4fb5a665b67663b7b6ca9272bc687a1e225d64aa
Marasigan-Paul-Foster-BSCS21A1 <133609831+babkkin@users.noreply.github.com>
2025-09-15 21:38:35 +0800

    Update page.js
    Changed Text Sizes
    
    8>7
    4>3
    4>2

--- app/about/page.js
+++ app/about/page.js
@@ -4,15 +4,14 @@ export default function AboutPage() {
   return (
 <div>
   <h1 className="text-8xl font-bold text-black mt-50 mb-6 max-w-130 mx-[44vh] ">How it all Happened?</h1>
-    <p className="text-4xl text-black mb-8 mx-[44vh]">
+    <p className="text-2xl text-black mb-8 mx-[44vh]">
     The grand purpose of DailyBlogs <br />Written by our Team
   </p>
     <br/>
     <br/>
-  <h1 className="text-8xl font-bold text-black mb-6 mx-[44vh]">Hey there! Welcome to DailyBlogs!</h1>
+  <h1 className="text-7xl font-bold text-black mb-6 mx-[44vh]">Hey there! Welcome to DailyBlogs!</h1>
 <br/>
-<br/>
-<p className="text-4xl text-black mb-8 mx-[44vh] text-justify">
+<p className="text-2xl text-black mb-8 mx-[44vh] text-justify">
   We started this website as a group of students working on a school project. At first, it was just about passing a subject—but along the way, we realized it could be something bigger.
   Instead of being just our personal blog, DailyBlogs is designed as a space where anyone can share their thoughts, stories, and ideas.<br /><br />
   For us, this project is a way to explore coding, learn how real websites work, and experiment with building something useful for others. But for you, it&apos;s a platform to express yourself.
@@ -29,8 +28,8 @@ export default function AboutPage() {
 <br/>
 <br/>
 <br/>
-<h1 className="text-8xl font-bold text-black mb-6 mx-[44vh]">Meet Our Team</h1>
-<p className="text-4xl text-black mb-8 mx-[44vh]">
+<h1 className="text-7xl font-bold text-black mb-6 mx-[44vh]">Meet Our Team</h1>
+<p className="text-2xl text-black mb-8 mx-[44vh]">
     People who made this website a reality
 </p>
 <br/>
@@ -48,10 +47,10 @@ export default function AboutPage() {
     />
   </div>
   <div className="flex-1">
-    <p className="text-4xl font-bold text-black text-justify">Ivan Sambat</p>
-    <p className="text-4xl text-black mb-4 text-justify">Full Stack Developer</p>
+    <p className="text-3xl font-bold text-black text-justify">Ivan Sambat</p>
+    <p className="text-2xl text-black mb-4 text-justify">Full Stack Developer</p>
     <br/>
-    <p className="text-4xl text-black text-justify">About me: I like both coding the front and back end, and I enjoy learning new things along the way. For me, this project is a chance to explore how everything fits together in building a website.</p>
+    <p className="text-2xl text-black text-justify">About me: I like both coding the front and back end, and I enjoy learning new things along the way. For me, this project is a chance to explore how everything fits together in building a website.</p>
   </div>
 </div>
 
@@ -67,10 +66,10 @@ export default function AboutPage() {
     />
   </div>
   <div className="flex-1">
-    <p className="text-4xl font-bold text-black text-justify">Paul Marasigan</p>
-    <p className="text-4xl text-black mb-4 text-justify">Frontend Developer</p>
+    <p className="text-3xl font-bold text-black text-justify">Paul Marasigan</p>
+    <p className="text-2xl text-black mb-4 text-justify">Frontend Developer</p>
     <br/>
-    <p className="text-4xl text-black text-justify">About me: I love working on layouts, colors, and everything that makes a site look good. I enjoy experimenting with designs and making sure the website feels smooth and easy to use.</p>
+    <p className="text-2xl text-black text-justify">About me: I love working on layouts, colors, and everything that makes a site look good. I enjoy experimenting with designs and making sure the website feels smooth and easy to use.</p>
   </div>
 </div>
 
@@ -86,10 +85,10 @@ export default function AboutPage() {
     />
   </div>
   <div className="flex-1">
-    <p className="text-4xl font-bold text-black text-justify">Jhune Magdurulan</p>
-    <p className="text-4xl text-black mb-4 text-justify">Backend Developer</p>
+    <p className="text-3xl font-bold text-black text-justify">Jhune Magdurulan</p>
+    <p className="text-2xl text-black mb-4 text-justify">Backend Developer</p>
     <br/>
-    <p className="text-4xl text-black text-justify">About me: I&apos;m passionate about problem-solving and making sure everything works behind the scenes. I like figuring out how to connect features and keep things running efficiently</p>
+    <p className="text-2xl text-black text-justify">About me: I&apos;m passionate about problem-solving and making sure everything works behind the scenes. I like figuring out how to connect features and keep things running efficiently</p>
   </div>
 </div>
 
@@ -105,10 +104,10 @@ export default function AboutPage() {
     />
   </div>
   <div className="flex-1">
-    <p className="text-4xl font-bold text-black text-justify">Jhon Alonzo</p>
-    <p className="text-4xl text-black mb-4 text-justify">Project Manager</p>
+    <p className="text-3xl font-bold text-black text-justify">Jhon Alonzo</p>
+    <p className="text-2xl text-black mb-4 text-justify">Project Manager</p>
     <br/>
-    <p className="text-4xl text-black text-justify">About me: I help organize tasks, keep the group on track, and make sure deadlines are met. I like planning things out and seeing ideas come to life through teamwork.</p>
+    <p className="text-2xl text-black text-justify">About me: I help organize tasks, keep the group on track, and make sure deadlines are met. I like planning things out and seeing ideas come to life through teamwork.</p>
   </div>
 </div>
 
@@ -124,10 +123,10 @@ export default function AboutPage() {
     />
   </div>
   <div className="flex-1">
-    <p className="text-4xl font-bold text-black text-justify">Nicolette Gonio</p>
-    <p className="text-4xl text-black mb-4 text-justify">Content Writer</p>
+    <p className="text-3xl font-bold text-black text-justify">Nicolette Gonio</p>
+    <p className="text-2xl text-black mb-4 text-justify">Content Writer</p>
     <br/>
-    <p className="text-4xl text-black text-justify">About me: I enjoy writing and sharing ideas through words. I like exploring different topics, putting them into blogs, and giving our site personality through content.</p>
+    <p className="text-2xl text-black text-justify">About me: I enjoy writing and sharing ideas through words. I like exploring different topics, putting them into blogs, and giving our site personality through content.</p>
   </div>
 </div>
 
