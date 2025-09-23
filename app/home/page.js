0bf80c143986b176015e95e3ad02327a762bc7a1
Marasigan-Paul-Foster-BSCS21A1 <133609831+babkkin@users.noreply.github.com>
2025-09-23 15:58:59 +0800

    Create page.js

---
+++ app/home/page.js
@@ -0,0 +1,9 @@
+export default function HomePage() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-50">
+      <h1 className="text-4xl font-bold text-gray-800">
+        🎉 Welcome to your Homepage!
+      </h1>
+    </div>
+  );
+}
