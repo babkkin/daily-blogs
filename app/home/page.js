e1d34c32cec352f7729da98e2d72257355fa3907
Babbkin <133609831+babkkin@users.noreply.github.com>
2025-10-11 16:50:03 +0800

    Update page.js

--- app/home/page.js
+++ app/home/page.js
@@ -5,16 +5,16 @@ import MediumSidebar from "@/components/Right-Sidebar";
 
 export default function HomePage() {
   return (
-    <div className="max-w-7xl mx-auto px-6 py-8">
+    <div className="max-w-7xl mx-auto px-6">
       <div className="flex gap-8">
         {/* Main Content - Left Side */}
-        <div className="flex-1">
+        <div className="flex-1 py-8">
           <Homeee />
         </div>
 
         {/* Sidebar - Right Side */}
-        <aside className="w-80 flex-shrink-0 hidden lg:block border-l border-gray-200">
-          <div className="sticky top-1">
+        <aside className="w-80 flex-shrink-0 hidden lg:block border-l border-gray-200 -my-8">
+          <div className="sticky top-20 py-8">
             <MediumSidebar />
           </div>
         </aside>
