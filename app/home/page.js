97bc51e16d500bcfb5d670ee37d5220c780833f7
Jhune Rey E. Magdurulan <mjhunerey28@gmail.com>
2025-10-31 15:44:26 +0800

    Scroll

--- app/home/page.js
+++ app/home/page.js
@@ -20,7 +20,7 @@ export default function HomePage() {
 
         {/* Sidebar (hidden on mobile) */}
         <aside className="w-full lg:w-80 flex-shrink-0 hidden lg:block border-t lg:border-t-0 lg:border-l border-gray-200 mt-6 lg:mt-0">
-          <div className="sticky top-20 py-6 lg:py-8">
+          <div className="sticky top-20 py-6 lg:py-8 max-h-[calc(100vh-5rem)] overflow-y-auto [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-track]:bg-transparent [&::-webkit-scrollbar-thumb]:bg-transparent hover:[&::-webkit-scrollbar-thumb]:bg-gray-300 [&::-webkit-scrollbar-thumb]:rounded-full">
             <MediumSidebar 
               onCategorySelect={setSelectedCategory} 
               selectedCategory={selectedCategory} 
